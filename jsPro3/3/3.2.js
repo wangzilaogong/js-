f06d8997e8684acd98a336636e1d396c8e9649ae
wangzilaogong <997972547@qq.com>
2017-06-07 00:06:23 +0800

    .

---
+++ jsPro3/3/3.2.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wk on 2017-06-06.
+ */
+
+//关键字和保留字
+/**
+ *这里照道理有很多的关键字和保留字
+ * 不同的ECMA版本不太一样
+ * ECMA5：保留字
+ *      class enum extends super
+ *      const export import
+ *       严格模式下
+ *         implements package public
+ *         interface private static
+ *         let  protected yield
+ *在使用过程中要注意版本的区别
+ *
+ */
---
+++ jsPro3/3/3.3.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wk on 2017-06-07.
+ */
+//变量
+//    var message ; 不赋值
+//    var amessage = "hi"; 赋值
+
