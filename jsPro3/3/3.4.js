78511ccc9d488e0967737ea3751622313e86a248
wangzilaogong <997972547@qq.com>
2017-06-13 23:42:06 +0800

    .

--- jsPro3/3/3.4.js
+++ jsPro3/3/3.4.js
@@ -56,7 +56,7 @@
 
 //3.4.5Number类型
     var intNum = 55;//int整型
-    //8进制
+    //8进制 一般0-7来表示，就和二进制差不多的，只是换了个数值范围
     var num1 = 070;//56
     var num2 = 079//无效的八进制 ，显示79
     var num3 = 08 //显示为8
@@ -73,4 +73,52 @@
     var floatNum2 = 0.1;
     var floatNum3 = .1;
 
-    
+    //2.数值范围
+    var min = Number.MIN_VALUE //5e-324
+    var max = Number.MAX_VALUE //
+    //在这个范围之外的数值将自动转换为infinity值有正负且无法参加正常的运算
+    var result = min+max;
+    alert(isFinite(result));//false
+
+    //3.NaN(NOT a Number)
+    //任何涉及到NaN的操作都会返回NaN 如NaN/10 且NaN与任何值不相等包括自己本身。
+    alert(NaN == NaN); //false
+
+    alert(isNaN(NaN));//true
+    alert(isNaN(10));//false  10当然是个数字了
+    alert(isNaN("10"));//false  字符串10当然不是数字了但是被转换成10...这就很好玩了。应该是括号内的被转换后的值进行函数操作
+    alert(isNaN("blue"));//true  blue 也是字符串
+    alert(isNaN(true));//false   被转换成1 妈的，不然都不知道。竟然还有这种操作的
+    //isNaN 还可以对对象进行操作，感觉还是有点莫名其妙，在对象操作过程中，先调用valueOf方法，确定返回值之后，是否可以转为数值，如果不能，则toString方法判断
+    //~这也就解释了上面的对于字符10的操作过程，这样就一目了然了。
+
+    //4.数值转换 Number() parseInt() parseFloat()
+    //4.1 Number()
+         //Boolean true 和false 分别转换为1和0
+         //数值  即便 是 传入和返回
+         //null  0
+         //undefined NaN Number(undefined) NaN
+         //字符串 的话比较复杂 "1" -> 1 "1243"->1243 "011"->11 忽略前面的0
+                  // "0.11"?忽略前面的0？等等去测试下  "1.1"->1.1
+                  //字符串里面包含16进制的话，也直接转换成16进制的然后在变成十进制;
+                  //如果是空字符串的话，当然是了
+                  //除此之外都是NaN
+
+    var num1 = Number("Hello World!"); //除此外系列所以是NaN
+    var num2 = Number(""); //0
+    var num3 = Number("000011"); //11
+    var num4 = Number(true); //1
+    //鉴于这个函数不够合理，所以我们经常用的是parseInt处理整数的时候。
+    var num5 = parseInt("12345blue"); //12345
+    var num6 = parseInt(""); //NaN
+    var num7 = parseInt("0xA"); //10
+    var num8 = parseInt("22.5"); //22
+    var num9 = parseInt("070"); //56 8进制
+    var num10 = parseInt("70"); //70 10进制
+    var num11 = parseInt("0xf"); //15 16进制
+    //parseInt在解析字符串的时候，尤其是八进制的时候ECMA3和5存在分歧。
+    //ECMA3
+     var numa = parseInt("070")//56 8进制
+    //ECMA5
+     var numa = parseInt("070")//70 10进制
+
