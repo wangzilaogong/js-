148b5e75b36a2fe54a49b05ec05ea04efd9f66d8
wangzilaogong <997972547@qq.com>
2017-07-05 22:27:40 +0800

    .

---
+++ jqmini/b.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wk on 2017-07-05.
+ */
+(function (window,document,$) {
+    window.utils = {
+           tmArray:{
+               each:function(arr,fn){
+                   
+               }
+           }
+    }
+
+})();
---
+++ jqmini/inputvalidate.js
@@ -0,0 +1,37 @@
+/**
+ * Created by wk on 2017-07-05.
+ */
+var inputValidate = {
+    isInt:function(a){
+        var reg = /^[0-9]*$/;
+        return reg.test(a);
+    },
+    isChinese:function(a){//中文的
+        var reg = /^[\u4e00-\u9fa5]{0,}$/;
+        return reg.test(a);
+    },
+    isEngNumber:function(a){//英文和数字的
+        var reg = /^[A-Za-z0-9]+$/;
+        return reg.test(a);
+    },
+    //isUnusual:function(a){// 可以输入含有^%&',;=?$\" 输入%返回false
+    //    var reg = /[^%&',;=?$\x22]+/;
+    //    return reg.test(a);
+    //}
+    isEmail:function(a){
+        var reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        return reg.test(a);
+    },
+    isphoneNum:function(a){
+        var reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+        return reg.test(a);
+    },
+    isCNE:function(){
+        var reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+        return reg.test(a);
+    }
+
+
+
+
+};
